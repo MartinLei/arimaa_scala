6a0173a7f9faac745323ffb6d0c588e60a75f093
MartinLei <martin.lei@htwg-konstanz.de>
2017-08-23 10:07:10 +0200

    add travis ci

--- src/main/scala/model/TileNameEnum.scala
+++ src/main/scala/model/TileNameEnum.scala
@@ -2,5 +2,10 @@ package model
 
 object TileNameEnum extends Enumeration {
   type TileNameEnum = Value
-  val RABBIT, CAT, DOG, HORSE, CAMEL, ELEPHANT = Value
+  val RABBIT = Value("R")
+  val CAT = Value("C")
+  val DOG = Value("D")
+  val HORSE = Value("H")
+  val CAMEL = Value("C")
+  val ELEPHANT = Value("E")
 }
