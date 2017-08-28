9fbe1f92e7ae9959c4bc89203ef78a3b9724de02
MartinLei <martin.lei@htwg-konstanz.de>
2017-08-28 18:03:06 +0200

    add versa func

--- src/main/scala/util/Coordinate.scala
+++ src/main/scala/util/Coordinate.scala
@@ -1,6 +1,13 @@
 package util
 
 object Coordinate {
+  def posToCoordinate(pos: Position): String = {
+    val sb = new StringBuilder
+    val xChar: Char = (pos.x + 96).toChar
+    sb.append(xChar)
+    sb.append(pos.y)
+    sb.toString()
+  }
 
   def toPosition(coordinates: String): Position = {
     val x: Int = coordinates.charAt(0).toInt - 96
--- src/test/scala/util/CoordinateSpec.scala
+++ src/test/scala/util/CoordinateSpec.scala
@@ -4,8 +4,13 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CoordinateSpec extends FlatSpec with Matchers {
 
-  "coordinatesToPosition" should "convert ( a1 - h1) to a Position object" in {
+  "toPosition" should "convert ( a1 - h1) to a Position object" in {
     Coordinate.toPosition("a1") should be(new Position(1, 1))
     Coordinate.toPosition("h8") should be(new Position(8, 8))
+  }
+
+  "posToCoordinate" should "convert position to coordinate" in {
+    Coordinate.posToCoordinate(new Position(1, 2)) should be("a2")
+    Coordinate.posToCoordinate(new Position(8, 8)) should be("h8")
   }
 }
