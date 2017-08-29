45999252125af976da4b639440518996b549b99e
MartinLei <martin.lei@htwg-konstanz.de>
2017-08-29 13:46:15 +0200

    fix coverage

--- src/test/scala/controller/message/WrongFromPosSpec.scala
+++ src/test/scala/controller/message/WrongFromPosSpec.scala
@@ -1,7 +1,9 @@
 package controller.message
 
-import controller.impl.messages.imp.WrongFromPosMessage
+import controller.impl.messages.MessageTrade
+import controller.impl.messages.imp.{MoveMessage, WrongFromPosMessage}
 import org.scalatest.{FlatSpec, Matchers}
+import util.Position
 
 class WrongFromPosSpec extends FlatSpec with Matchers {
 
@@ -20,4 +22,25 @@ class WrongFromPosSpec extends FlatSpec with Matchers {
     message.text should be("Your first Coordinate ist wrong")
   }
 
+
+  "equals" should "be if its same class" in {
+    val wrongFromPos1: MessageTrade = new WrongFromPosMessage()
+    val wrongFromPos2: MessageTrade = new WrongFromPosMessage()
+
+    wrongFromPos1 should be(wrongFromPos2)
+  }
+
+  it should "not be if its not same class" in {
+    val wrongFromPos1: MessageTrade = new WrongFromPosMessage()
+    val moveMessage: MessageTrade = new MoveMessage(new Position(1, 1), new Position(1, 2))
+
+    wrongFromPos1 should not be moveMessage
+  }
+
+  it should "not be if its not from MessageTrade" in {
+    val wrongFromPos1: MessageTrade = new WrongFromPosMessage()
+    val testString: String = "Test"
+
+    wrongFromPos1 should not be testString
+  }
 }
