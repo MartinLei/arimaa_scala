d05d13d6ca54b7384d13211f5aff0ea8bda2647b
MartinLei <martin.lei@htwg-konstanz.de>
2017-08-30 10:32:23 +0200

    impl get surround points of a point

--- src/test/scala/util/PositionSpec.scala
+++ src/test/scala/util/PositionSpec.scala
@@ -43,4 +43,25 @@ class PositionSpec extends FlatSpec with Matchers {
     val pos1: Position = new Position(1, 2)
     pos1 should not be 1
   }
+
+  "getSurround" should "be get a set of surrounded position" in {
+    val surround3_3Should = Set(new Position(3, 4), new Position(4, 3), new Position(3, 2), new Position(2, 3))
+    val pos3_3 = new Position(3, 3)
+
+    Position.getSurround(pos3_3) should be(surround3_3Should)
+  }
+
+  it should "get only two in a corner" in {
+    val surround1_1Should = Set(new Position(2, 1), new Position(1, 2))
+    val pos1_1 = new Position(1, 1)
+
+    Position.getSurround(pos1_1) should be(surround1_1Should)
+  }
+
+  it should "get only three on a side" in {
+    val surround1_3Should = Set(new Position(1, 4), new Position(2, 3), new Position(1, 2))
+    val pos1_3 = new Position(1, 3)
+
+    Position.getSurround(pos1_3) should be(surround1_3Should)
+  }
 }
