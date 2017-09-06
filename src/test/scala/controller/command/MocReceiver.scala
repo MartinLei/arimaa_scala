416f2ab03900437ff26647f16d7a6422a742ee98
MartinLei <martin.lei@htwg-konstanz.de>
2017-09-06 10:11:06 +0200

    remove unused mocs

--- src/test/scala/controller/command/MocReceiver.scala
+++
@@ -1,9 +0,0 @@
-package controller.command
-
-import util.position.Position
-
-class MocReceiver(var pos: Position) {
-  def add(value: Int): Unit = {
-    pos = new Position(pos.x + value, pos.y + value)
-  }
-}
