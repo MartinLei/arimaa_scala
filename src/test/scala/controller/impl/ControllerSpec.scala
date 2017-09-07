044f2d5de3b6781e7263e96d54e4114339ab0132
MartinLei <martin.lei@htwg-konstanz.de>
2017-09-07 15:19:03 +0200

    add isPull rule to controller spec

--- src/test/scala/controller/impl/ControllerSpec.scala
+++ src/test/scala/controller/impl/ControllerSpec.scala
@@ -173,6 +173,35 @@ class ControllerSpec extends FlatSpec with Matchers {
     controller.getTileName(PlayerNameEnum.GOLD, new Position(2, 3)) should be(TileNameEnum.HORSE)
     controller.getTileName(PlayerNameEnum.GOLD, new Position(3, 3)) should be(TileNameEnum.CAT)
   }
+
+  it should "pull a other player tile, if it surround by player stronger tile" in {
+    val controller = new Controller()
+
+    controller.moveTile(new Position(5, 2), new Position(5, 3)) should
+      be(List(new MoveMessage(new Position(5, 2), new Position(5, 3))))
+    controller.moveTile(new Position(5, 3), new Position(5, 4)) should
+      be(List(new MoveMessage(new Position(5, 3), new Position(5, 4))))
+
+    controller.changePlayer()
+
+    controller.moveTile(new Position(5, 7), new Position(5, 6)) should
+      be(List(new MoveMessage(new Position(5, 7), new Position(5, 6))))
+    controller.moveTile(new Position(5, 6), new Position(5, 5)) should
+      be(List(new MoveMessage(new Position(5, 6), new Position(5, 5))))
+
+    controller.changePlayer()
+
+    controller.getTileName(PlayerNameEnum.GOLD, new Position(5, 4)) should be(TileNameEnum.ELEPHANT)
+    controller.getTileName(PlayerNameEnum.SILVER, new Position(5, 5)) should be(TileNameEnum.CAMEL)
+
+    controller.moveTile(new Position(5, 5), new Position(6, 5)) should
+      be(List(new PullMessage(new Position(5, 5), new Position(6, 5))))
+
+    controller.getTileName(PlayerNameEnum.GOLD, new Position(5, 4)) should be(TileNameEnum.ELEPHANT)
+    controller.getTileName(PlayerNameEnum.SILVER, new Position(5, 5)) should be(TileNameEnum.NONE)
+    controller.getTileName(PlayerNameEnum.SILVER, new Position(5, 6)) should be(TileNameEnum.CAMEL)
+  }
+
   "changePlayer" should "change the Player" in {
     val controller: ControllerTrait = new Controller()
     controller.getActPlayerName should be(PlayerNameEnum.GOLD)
