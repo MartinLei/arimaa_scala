86362120a19a9a10491d7f9297fe2e81a88112e9
MartinLei <martin.lei@htwg-konstanz.de>
2017-09-12 10:42:01 +0200

    impr. test

--- src/test/scala/controller/impl/ControllerSpec.scala
+++ src/test/scala/controller/impl/ControllerSpec.scala
@@ -238,24 +238,11 @@ class ControllerSpec extends FlatSpec with Matchers {
       be(List(Message.pushNotFinish))
   }
   it should "pull a other player tile, if posTo is the same as the old posFrom last moved tile" in {
-    val controller = new Controller()
-
-    controller.moveTile(new Position(5, 2), new Position(5, 3)) should
-      be(List(Message.doMove(new Position(5, 2), new Position(5, 3))))
-    controller.moveTile(new Position(5, 3), new Position(5, 4)) should
-      be(List(Message.doMove(new Position(5, 3), new Position(5, 4))))
-
-    controller.changePlayer()
-
-    controller.moveTile(new Position(5, 7), new Position(5, 6)) should
-      be(List(Message.doMove(new Position(5, 7), new Position(5, 6))))
-    controller.moveTile(new Position(5, 6), new Position(5, 5)) should
-      be(List(Message.doMove(new Position(5, 6), new Position(5, 5))))
-
-    controller.changePlayer()
-
-    controller.getTileName(PlayerNameEnum.GOLD, new Position(5, 4)) should be(TileNameEnum.ELEPHANT)
-    controller.getTileName(PlayerNameEnum.SILVER, new Position(5, 5)) should be(TileNameEnum.CAMEL)
+    val playerGoldTiles = Set(
+      new Tile(TileNameEnum.ELEPHANT, new Position(5, 4)))
+    val playerSilverTiles = Set(
+      new Tile(TileNameEnum.CAMEL, new Position(5, 5)))
+    val controller = new Controller(playerGoldTiles, playerSilverTiles)
 
     controller.moveTile(new Position(5, 4), new Position(6, 4)) should
       be(List(Message.doMove(new Position(5, 4), new Position(6, 4))))
