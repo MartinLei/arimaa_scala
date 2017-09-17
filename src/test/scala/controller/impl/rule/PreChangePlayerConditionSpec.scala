d3a5512d995a5e3c510552688bf2eabf4271c445
MartinLei <martin.lei@htwg-konstanz.de>
2017-09-17 12:13:06 +0200

    fix test

--- src/test/scala/controller/impl/rule/PreChangePlayerConditionSpec.scala
+++ src/test/scala/controller/impl/rule/PreChangePlayerConditionSpec.scala
@@ -10,26 +10,26 @@ class PreChangePlayerConditionSpec extends FlatSpec with Matchers {
 
   "isPushNotFinish" should "true if, last command is a push command" in {
     val playerGoldTiles = Set(
-      new Tile(TileNameEnum.HORSE, new Position(5, 4)))
+      new Tile(TileNameEnum.CAMEL, new Position(5, 4)))
     val playerSilverTiles = Set(
-      new Tile(TileNameEnum.CAMEL, new Position(5, 5)))
+      new Tile(TileNameEnum.HORSE, new Position(5, 5)))
     val field = new Field(playerGoldTiles, playerSilverTiles)
 
     val actionManager = new ActionManager()
-    val action = new ActionCommand(List(PushCommand(field, PlayerNameEnum.GOLD, new Position(5, 5), new Position(6, 5))))
+    val action = new ActionCommand(List(PushCommand(field, PlayerNameEnum.SILVER, new Position(5, 5), new Position(6, 5))))
     actionManager.doAction(action)
 
     PreChangePlayerCondition.isPushNotFinish(actionManager) should be(true)
   }
   it should "false, if not" in {
     val playerGoldTiles = Set(
-      new Tile(TileNameEnum.HORSE, new Position(5, 4)))
+      new Tile(TileNameEnum.CAMEL, new Position(5, 4)))
     val playerSilverTiles = Set(
-      new Tile(TileNameEnum.CAMEL, new Position(5, 5)))
+      new Tile(TileNameEnum.HORSE, new Position(5, 5)))
     val field = new Field(playerGoldTiles, playerSilverTiles)
 
     val actionManager = new ActionManager()
-    val action1 = new ActionCommand(List(PushCommand(field, PlayerNameEnum.GOLD, new Position(5, 5), new Position(6, 5))))
+    val action1 = new ActionCommand(List(PushCommand(field, PlayerNameEnum.SILVER, new Position(5, 5), new Position(6, 5))))
     val action2 = new ActionCommand(List(MoveCommand(field, PlayerNameEnum.GOLD, new Position(5, 4), new Position(5, 5))))
     actionManager.doAction(action1)
     actionManager.doAction(action2)
