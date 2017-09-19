f29b54f7856b0a51ddfef43f22f426077832ee3c
MartinLei <martin.lei@htwg-konstanz.de>
2017-09-19 08:18:05 +0200

    fix test

--- src/test/scala/controller/impl/command/TurnManagerSpec.scala
+++ src/test/scala/controller/impl/command/TurnManagerSpec.scala
@@ -1,6 +1,6 @@
 package controller.impl.command
 
-import controller.impl.command.impl.MoveCommand
+import controller.impl.command.impl.{MoveCommand, TrapCommand}
 import controller.impl.messages.MessageText
 import model.impl.{Field, PlayerNameEnum, Tile, TileNameEnum}
 import org.scalatest.{FlatSpec, Matchers}
@@ -45,16 +45,20 @@ class TurnManagerSpec extends FlatSpec with Matchers {
   }
   "getActualPlayerLastActionPosFrom" should "get the players last action pos From" in {
     val playerGoldTiles = Set(
-      new Tile(TileNameEnum.RABBIT, new Position(1, 1)))
+      new Tile(TileNameEnum.RABBIT, new Position(1, 1)),
+      new Tile(TileNameEnum.RABBIT, new Position(2, 1)))
     val field = new Field(playerGoldTiles, Set())
     val actionCommand = ActionCommand(List(
-      MoveCommand(field, PlayerNameEnum.GOLD, new Position(1, 1), new Position(1, 2))))
+      MoveCommand(field, PlayerNameEnum.GOLD, new Position(1, 1), new Position(1, 2)),
+      TrapCommand(field, PlayerNameEnum.GOLD, new Position(2, 1))))
 
     val turnManager = new TurnManager
     turnManager.addTurn(PlayerNameEnum.GOLD)
 
     turnManager.doAction(actionCommand) should
-      be(List(MessageText.doMove(new Position(1, 1), new Position(1, 2))))
+      be(List(
+        MessageText.doMove(new Position(1, 1), new Position(1, 2)),
+        MessageText.doTrap(new Position(2, 1))))
     turnManager.getActualPlayerLastActionPosFrom should be(Some(new Position(1, 1)))
   }
   it should "null if stack is empty" in {
@@ -65,16 +69,20 @@ class TurnManagerSpec extends FlatSpec with Matchers {
   }
   "getActualPlayerLastActionPosTo" should "get the players last action pos To" in {
     val playerGoldTiles = Set(
-      new Tile(TileNameEnum.RABBIT, new Position(1, 1)))
+      new Tile(TileNameEnum.RABBIT, new Position(1, 1)),
+      new Tile(TileNameEnum.RABBIT, new Position(2, 1)))
     val field = new Field(playerGoldTiles, Set())
     val actionCommand = ActionCommand(List(
-      MoveCommand(field, PlayerNameEnum.GOLD, new Position(1, 1), new Position(1, 2))))
+      MoveCommand(field, PlayerNameEnum.GOLD, new Position(1, 1), new Position(1, 2)),
+      TrapCommand(field, PlayerNameEnum.GOLD, new Position(2, 1))))
 
     val turnManager = new TurnManager
     turnManager.addTurn(PlayerNameEnum.GOLD)
 
     turnManager.doAction(actionCommand) should
-      be(List(MessageText.doMove(new Position(1, 1), new Position(1, 2))))
+      be(List(
+        MessageText.doMove(new Position(1, 1), new Position(1, 2)),
+        MessageText.doTrap(new Position(2, 1))))
     turnManager.getActualPlayerLastActionPosTo should be(Some(new Position(1, 2)))
   }
   it should "null if stack is empty" in {
@@ -83,6 +91,7 @@ class TurnManagerSpec extends FlatSpec with Matchers {
 
     turnManager.getActualPlayerLastActionPosFrom should be(Option(null))
   }
+
 }
 
 
